22aa1367bcb034fe433b584c7738aee3c5fb3c17
Kayode Taiwo <kay_taylor@outlook.com>
2021-10-22 02:25:31 -0500

    changes to home page v3

--- src/home/App.js
+++ src/home/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
                 <TopNav />
                 <div className="bg">
                     <div className="middle">
-                        <h1>SOFTWARE DEVELOPMENT AND DATA SCIENCES</h1>
+                        <h1>SOFTWARE DEVELOPMENT, DATA SCIENCES, AND MACHINE LEARNING</h1>
                         <h3>coming soon in</h3>
                         <p style={{ fontSize:30 }}>
                             {this.state.days}d {this.state.hours}h {this.state.minutes}m {this.state.seconds}s
