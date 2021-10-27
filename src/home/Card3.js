c00c6759ee48a7b88008808827dce521bf73f65b
Kayode Taiwo <kay_taylor@outlook.com>
2021-10-27 09:25:52 -0500

    more v1

---
+++ src/home/Card3.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Card3.css';
+
+class Card3 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: false };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+    
+    render() {
+        return (
+            <div className="Card3_card3">
+                <h2 className="Card3_title">{this.props.title}</h2>
+                <p>
+                    <img
+                        alt=""
+                        src={this.props.img}
+                        width="100%" />
+                </p>
+                <p className="Card3_body">{this.state.isToggleOn ? this.props.children : this.props.children.substring(0, 150) + '...'}</p>
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? 'LESS' : 'MORE'}
+                </button>
+            </div>
+        );
+    }
+}
+  
+export default Card3;
