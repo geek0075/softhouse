fd079671cab30823d86ebac1f02e649a3c4a22b7
Kayode Taiwo <kay_taylor@outlook.com>
2021-11-01 15:14:05 -0500

    team v4

--- src/home/Team.js
+++ src/home/Team.js
@@ -1,8 +1,8 @@
 import './Team.css';
 
 import team1 from './img/kay01.jpg';
-import team2 from './img/kay01.jpg';
-import team3 from './img/kay01.jpg';
+import team2 from './img/him.jpeg';
+import team3 from './img/her.jpeg';
 
 function Team() {
     return (
@@ -28,10 +28,10 @@ function Team() {
                         <div className="Home_Team_card">
                             <img src={team2} alt="Mike" style={{ width : "100%" }} />
                             <div className="Home_Team_inner_container">
-                                <h2>Kayode Taiwo</h2>
-                                <p className="Home_Team_subtitle">Art Director</p>
-                                <p>Doing Data Analysis, Machine Learning, and Deep Learning with R, Python, Numpy, Scipy, Sci kit-Learn, PyTorch, and Tensorflow on Jupiter Notebooks,</p>
-                                <p>kt.taylor007@gmail.com</p>
+                                <h2>Coming Soon</h2>
+                                <p className="Home_Team_subtitle">Software Programmer</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <p>you@softhouse-llc.com</p>
                                 <p><button className="Home_Team_button">Contact</button></p>
                             </div>
                         </div>
@@ -40,10 +40,10 @@ function Team() {
                         <div class="Home_Team_card">
                             <img src={team3} alt="John" style={{ width : "100%" }} />
                             <div className="Home_Team_inner_container">
-                                <h2>Kayode Taiwo</h2>
+                                <h2>Coming Soon</h2>
                                 <p className="Home_Team_subtitle">Designer</p>
-                                <p>Doing Data Analysis, Machine Learning, and Deep Learning with R, Python, Numpy, Scipy, Sci kit-Learn, PyTorch, and Tensorflow on Jupiter Notebooks,</p>
-                                <p>kt.taylor007@gmail.com</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <p>you@softhouse-llc.com</p>
                                 <p><button className="Home_Team_button">Contact</button></p>
                             </div>
                         </div>
